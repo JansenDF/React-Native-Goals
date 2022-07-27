322eff9a0362dc73c7f3ef7141ed711f7d20ce7a
JansenDF <jansen.lima.df@gmail.com>
2022-07-27 11:35:05 -0300

    Adicionado componente Text e Button, e Style dummyText

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World! Perdeu???</Text>
+      <View>
+        <Text style={styles.dummyText}>Other text!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World! Perdeu??!</Text>
       <StatusBar style="auto" />
+      <Button title='Tap Me!' />
     </View>
   );
 }
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: '#c4c4c4',
+  }
 });
