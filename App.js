a919dc60a0cf97a1935c7625f4e5ab6f16cad429
JansenDF <jansen.lima.df@gmail.com>
2022-07-27 11:37:33 -0300

    Reset do template para iniciar o projeto

--- App.js
+++ App.js
@@ -3,28 +3,13 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Other text!</Text>
-      </View>
-      <Text style={styles.dummyText}>Hello World! Perdeu??!</Text>
-      <StatusBar style="auto" />
-      <Button title='Tap Me!' />
+    <View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: '#c4c4c4',
-  }
+
 });
