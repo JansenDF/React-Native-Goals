5c145a5b4100db1ad50ce1156e0413f64ab24b22
JansenDF <jansen.lima.df@gmail.com>
2022-07-28 15:36:35 -0300

    Componetização do GoalsItem

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   FlatList,
 } from "react-native";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -41,11 +42,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />;
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -89,14 +86,5 @@ const styles = StyleSheet.create({
   },
   goalList: {
     margin: 8,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
